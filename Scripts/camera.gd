8b2a0264492ec16ad3c9273796d9a67ced7ead34
govr017 <govr017@gmail.com>
2024-01-21 00:27:44 +0500

    refinement

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -3,9 +3,11 @@ extends Camera2D
 
 func _on_1_body_entered(body):
 	if body.name == "Player":
+		$Label.text = "2000"
 		self.position = Vector2(80,-80)
 
 
 func _on_0_body_entered(body):
 	if body.name == "Player":
+		$Label.text = "1000"
 		self.position = Vector2(80,80)
